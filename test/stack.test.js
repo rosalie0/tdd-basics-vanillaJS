ef4d8868577b424c8dc1c99b334c1314552691c1
Rosalie Newman <rosalienewman0@gmail.com>
2023-04-11 19:07:01 -0400

    jest broke?

---
+++ test/stack.test.js
@@ -0,0 +1,16 @@
+// This is a test suite.
+describe("My Stack", () => {
+  // This is a singular test in this suite, with the string being a description of it.
+  // The 2nd arg is the function it runs.
+  // You can write it.todo if you've not written the test yet.
+
+  it("is created empty", () => {
+    const stack = new Stack();
+    // We are 'expecting' the top of an initialized stack to be -1.
+    expect(stack.top).toBe(-1);
+  });
+
+  it.todo("can push onto the top");
+
+  it.todo("can pop off from the top");
+});
